96c89e3cf798906515875c24326d44012873cd2e
Artem Klevtsov <a.a.klevtsov@gmail.com>
2014-09-04 01:18:48 +0700

    Update get-data.R

--- get-data.R
+++ get-data.R
@@ -1,23 +1,20 @@
-# Load libraries
-library(jsonlite)
 # Request URL
 url <- "https://www.googleapis.com/analytics/v3/metadata/ga/columns"
 # Get JSON data
-data.json <- fromJSON(url)
+data_json <- jsonlite::fromJSON(url)
 # Subsettings
-data.r <- .subset2(data.json, "items")
-id <- .subset2(data.r, "id")
-attributes <- .subset2(data.r, "attributes")
+data_r <- .subset2(data_json, "items")
+id <- .subset2(data_r, "id")
+attributes <- .subset2(data_r, "attributes")
+# Convert column types
 attributes  <- transform(attributes,
-    allowedInSegments = as.logical(match(allowedInSegments, table = c(NA, "true")) - 1),
-    minTemplateIndex = as.numeric(minTemplateIndex),
-    maxTemplateIndex = as.numeric(maxTemplateIndex),
-    premiumMinTemplateIndex = as.numeric(premiumMinTemplateIndex),
-    premiumMaxTemplateIndex = as.numeric(premiumMaxTemplateIndex)
+                         allowedInSegments = as.logical(match(allowedInSegments, table = c(NA, "true")) - 1),
+                         minTemplateIndex = as.numeric(minTemplateIndex),
+                         maxTemplateIndex = as.numeric(maxTemplateIndex),
+                         premiumMinTemplateIndex = as.numeric(premiumMinTemplateIndex),
+                         premiumMaxTemplateIndex = as.numeric(premiumMaxTemplateIndex)
 )
 # Create data.frame
 ga <- cbind(id, attributes, stringsAsFactors = FALSE)
-# Get data file path
-data_file <- file.path("data/ga.rda")
-# Save data to file
+# Save dataset
 saveRDS(ga, file = "ga.rds", compress = "xz")
